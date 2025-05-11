c1b6018adf6264e49fb9dd3efeccfc9de2a4cedf
1r0ger3 <64908927+1r0ger3@users.noreply.github.com>
2025-05-11 19:09:42 -0400

    Create comandos_gib.sh

---
+++ comandos_gib.sh
@@ -0,0 +1,10 @@
+# Inicializar repositorio
+git init
+
+# Crear repositorio en GitHub y conectar
+git remote add origin [URL_DEL_REPOSITORIO]
+
+# Primer commit
+git add .
+git commit -m "Estructura inicial del proyecto"
+git push -u origin main
